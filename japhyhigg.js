47725c0b834f52ef30ffbde0545d840dcef2e142
JaphyHigg <70935772+JaphyHigg@users.noreply.github.com>
2022-01-16 13:36:31 -0800

    Add files via upload

--- japhyhigg.js
+++ japhyhigg.js
@@ -3,6 +3,6 @@ const btns = document.querySelectorAll(".catagory");
 btns.forEach(function(btn){
     btn.addEventListener('click', function (e) {
         const dropdownTitle = e.currentTarget.parentElement;
-        dropdownTitle.classList.toggle('show-dropdown')
+        dropdownTitle.classList.toggle('show-dropdown');
     })
 });
